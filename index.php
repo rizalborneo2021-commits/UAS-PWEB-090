87d4bb07ae7aaaa57543ee6dae55fe0e78bc018d
rizalborneo2021-commits <rizalborneo2021@gmail.com>
2026-06-28 19:37:18 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Sistem Inventaris Barang</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="container container-mini" style="margin-top: 10vh;">
+    <h1 style="text-align: center;">Sistem Inventaris</h1>
+    <p style="text-align: center;">Selamat datang di aplikasi pendataan barang toko. Silakan pilih menu manajemen di bawah:</p>
+    
+    <a href="daftar.php" class="btn btn-primary" style="margin-bottom: 15px;">📦 Lihat Daftar Barang</a>
+    <a href="tambah.php" class="btn btn-success">➕ Tambah Barang Baru</a>
+</div>
+
+</body>
+</html>
